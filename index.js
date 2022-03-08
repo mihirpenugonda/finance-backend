9650bf137b063eb46876937297f1d5ebf6558feb
Mihr Penugonda <mihirpenugonda@gmail.com>
2022-03-08 10:24:39 +0000

    heroku test

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const { connectDatabase } = require("./config/database");
 
-const port = process.env.PORT || "5000";
-
-app.listen(port, () => {
-  console.log(`Server is working on http://localhost:${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server is working on http://localhost:${process.env.PORT || 3000}`
+  );
 });
 
 connectDatabase();
