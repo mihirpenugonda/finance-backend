f11ed04fa1b13c02f3e16442d5ae31dc2d0be296
Mihr Penugonda <mihirpenugonda@gmail.com>
2022-05-18 18:45:43 +0530

    fixed .env config

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const { connectDatabase } = require("./config/database");
 
+dotenv.config({
+  path: "./config/config.env",
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(
     `Server is working on http://localhost:${process.env.PORT || 3000}`
